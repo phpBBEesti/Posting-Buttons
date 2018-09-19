ed06b77e892907af3f7ab14e0e8aaaf7e6edfe83
phpBBEesti <argol76@gmail.com>
2018-09-19 12:25:28 +0300

    Update postingbuttons.php

--- dmzx/postingbuttons/language/et/postingbuttons.php
+++ dmzx/postingbuttons/language/et/postingbuttons.php
@@ -1,5 +1,6 @@
 <?php
 /**
+* Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu>
 *
 * Posting Buttons extension for the phpBB Forum Software package.
 *
@@ -38,5 +39,5 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-   'CUSTOM_BBCODES'   => 'Custom BBCodes',
+   'CUSTOM_BBCODES'   => 'Kohandatud BBkoodid',
 ));
